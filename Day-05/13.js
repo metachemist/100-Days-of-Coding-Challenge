d5e216bfc0663e826d7e50519fc14530c3715884
metachemist <meta.chemist.22@gmail.com>
2024-04-27 23:33:14 +0500

    Adding Question 13

---
+++ Day-05/13.js
@@ -0,0 +1,6 @@
+"use strict";
+let favTransports = ["Porsche 991", "Kawasaki ninja H2R", "Mclaren P1"];
+//lets not use for loop 
+favTransports.forEach(favTransport => {
+    console.log(`I have ${favTransport}.`);
+});
---
+++ Day-05/13.ts
@@ -0,0 +1,7 @@
+let favTransports: string[] = ["Porsche 991", "Kawasaki ninja H2R", "Mclaren P1"];
+
+//lets not use for loop 
+
+favTransports.forEach(favTransport =>{
+    console.log(`I have ${favTransport}.`)
+});
