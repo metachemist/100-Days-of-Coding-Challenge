b5599917f4d77d0f5c595d954a457c06d892aaca
metachemist <meta.chemist.22@gmail.com>
2024-04-27 23:04:05 +0500

    Adding Question 12

---
+++ Day-04/12.js
@@ -0,0 +1,5 @@
+"use strict";
+let names01 = ["hamnah", "asifa", "umaima", "shafay"];
+for (let i = 0; i < names01.length; i++) {
+    console.log(`hi ${names01[i]}, would you like to take tea?`);
+}
---
+++ Day-04/12.ts
@@ -0,0 +1,5 @@
+let names01: string[] = ["hamnah","asifa", "umaima", "shafay"];
+for (let i=0; i<names01.length; i++){
+   console.log(`hi ${names01[i]}, would you like to take tea?`)
+
+}
