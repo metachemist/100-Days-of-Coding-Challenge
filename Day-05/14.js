15a4e3785028acc714d8da48563849ee1f024b40
metachemist <meta.chemist.22@gmail.com>
2024-04-28 09:08:41 +0500

    Adding Question 14

---
+++ Day-05/14.js
@@ -0,0 +1,5 @@
+"use strict";
+let guests = ["Shafay", "Hamnah", "Khadija"];
+guests.forEach(guest => {
+    console.log(`Thank you ${guest} for coming at my place <3`);
+});
---
+++ Day-05/14.ts
@@ -0,0 +1,4 @@
+let guests: string[] = ["Shafay", "Hamnah", "Khadija"];
+guests.forEach(guest => {
+    console.log(`Thank you ${guest} for coming at my place <3`)
+});
